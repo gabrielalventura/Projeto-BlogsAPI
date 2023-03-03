da4128ada7e4d301ee72d1d4740fcfba5bf995d7
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-03 16:48:30 -0300

    Exec: Defini campos da tabela users na migration

--- src/migrations/20230303184750-create-users.js
+++ src/migrations/20230303184750-create-users.js
@@ -2,20 +2,34 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'display_name' // é preciso definir explicitamente o field porque se trata de um nome composto. baseado nas materias do course.
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('users');
+  },
 };
