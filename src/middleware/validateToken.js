6bd9cc0246e8b53448680572434226205e66182c
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-05 16:38:30 -0300

    Create: Cria middleware para validar token gerado no login

---
+++ src/middleware/validateToken.js
@@ -0,0 +1,17 @@
+const { verifyToken } = require('../auth/authFunctions');
+
+const validateToken = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return res.status(401).json({ message: 'Not authorized' });
+    }
+    const payload = verifyToken(authorization);
+    req.data = payload.data;
+    next();
+  } catch (error) {
+    res.status(500).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = validateToken;
