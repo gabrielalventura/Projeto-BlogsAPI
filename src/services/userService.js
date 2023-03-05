90bc4b851ca08f1df1bc221e5c20efa9eadcabd8
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-05 19:44:53 -0300

    Create: Cria função getAll em userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,6 +3,9 @@ const { User } = require('../models');
 const createUser = ({ displayName, email, password, image }) => 
 User.create({ displayName, email, password, image });
 
+const getUser = () => User.findAll();
+
 module.exports = {
   createUser,
+  getUser,
 };
