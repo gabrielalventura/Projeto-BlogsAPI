c9da8d1feb5b4606a98a2101751c0f29e085cc04
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-05 16:57:57 -0300

    Create: Cria loginController

---
+++ src/controllers/loginController.js
@@ -0,0 +1,29 @@
+const { createToken } = require('../auth/authFunctions');
+const { loginService } = require('../services');
+require('dotenv/config');
+
+const isBodyValid = (email, password) => email && password;
+
+module.exports = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!isBodyValid(email, password)) {
+      return res.status(401).json({ message: 'Some required fields are missing' });
+    }
+
+    const user = await loginService({ email, password });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid fields' });
+    }
+
+    const { password: _, ...userWithoutPassword } = user.dataValues;
+
+    const token = createToken(userWithoutPassword);
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal error' });
+  }
+};
