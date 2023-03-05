9b8f734e3b8a368262aead031fc6a662c704939d
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-05 17:19:18 -0300

    Fix: Corrige res status trocando de unauthorized para bad request

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -9,16 +9,16 @@ module.exports = async (req, res) => {
     const { email, password } = req.body;
 
     if (!isBodyValid(email, password)) {
-      return res.status(401).json({ message: 'Some required fields are missing' });
+      return res.status(400).json({ message: 'Some required fields are missing' });
     }
 
     const user = await loginService({ email, password });
 
     if (!user) {
-      return res.status(401).json({ message: 'Invalid fields' });
+      return res.status(400).json({ message: 'Invalid fields' });
     }
 
-    const { password: _, ...userWithoutPassword } = user.dataValues;
+    const { password: _, ...userWithoutPassword } = user;
 
     const token = createToken(userWithoutPassword);
 
