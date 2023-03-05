4459332fc72b156347136cc96e2fcde4f645b746
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-03-05 17:08:11 -0300

    Create: Cria a rota login que verifica o login e gera o token (ainda sem testes)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
-
-// ...
+const { loginRouter } = require('./routes');
 
 const app = express();
 
@@ -11,7 +10,7 @@ app.get('/', (_request, response) => {
 
 app.use(express.json());
 
-// ...
+app.use('/login', loginRouter);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
---
+++ src/routes/login.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const loginController = require('../controllers');
+// const validateToken = require('../middleware/validateToken');
+
+const router = express.Router();
+
+router.post('/', loginController);
+
+module.exports = router;
